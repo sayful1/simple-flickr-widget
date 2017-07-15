c22ece6b603dd1b1df2878a2859f972d33fe7526
Sayful Islam <sayful.islam001@gmail.com>
2017-07-15 18:03:18 +0600

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,71 @@
+=== Simple Flickr Widget ===
+Contributors: sayful
+Tags: flickr, photos, widget, feeds, flickr feeds
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3LZWQTHEVYWCY
+Requires at least: 3.5
+Tested up to: 4.8
+Stable tag: 1.2.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A widget which will display your latest Flickr photos.
+
+== Description ==
+This plugin makes it easy to pull Flickr feeds and display them on your site.
+
+= Usages =
+
+* After installing and activating the plugin, go to **WordPress Admin >> Appearance >> Widgets**
+* Find "Simple Flickr Widget" and drug and drop this widget at which widget area you want to show it.
+* You need Flickr ID number, To get Flickr ID number, go to your **Flickr account >> photostream** Now Look at you addressbar, it look like the following **https://www.flickr.com/photos/937xxxx5@N02/**
+* Just copy the Flickr ID like this **937xxxx5@N02** and paste this at Flickr ID number field at widget area and set others options as your need.
+
+== Installation ==
+Installing the plugins is just like installing other WordPress plugins. If you don't know how to install plugins, please review the two options below:
+
+Install by Search
+
+* From your WordPress dashboard, choose 'Add New' under the 'Plugins' category.
+* Search for 'simple-flickr-widget' a plugin will come called 'Simple Flickr Widget' and Click 'Install Now' and confirm your installation by clicking 'ok'
+* The plugin will download and install. Just click 'Activate Plugin' to activate it.
+
+Install by ZIP File
+
+* From your WordPress dashboard, choose 'Add New' under the 'Plugins' category.
+* Select 'Upload' from the set of links at the top of the page (the second link)
+* From here, browse for the zip file included in your plugin titled 'simple-flickr-widget.zip' and click the 'Install Now' button
+* Once installation is complete, activate the plugin to enable its features.
+
+Install by FTP
+
+* Find the directory titles 'upcoming-events-lists' and upload it and all files within to the plugins directory of your WordPress install (WORDPRESS-DIRECTORY/wp-content/plugins/) [e.g. http://www.yourdomain.com/wp-content/plugins/
+* From your WordPress dashboard, choose 'Installed Plugins' option under the 'Plugins' category
+* Locate the newly added plugin and click on the 'Activate' link to enable its features.
+
+
+== Frequently Asked Questions ==
+Do you have questions or issues with Simple Flickr Widget? [Ask for support](http://wordpress.org/support/plugin/simple-flickr-widget)
+
+== Screenshots ==
+1. Screenshot of Simple Flickr Widget
+2. Screenshot of Simple Flickr Widget Front-end
+
+== Changelog ==
+
+= 1.2.0 - 2017-06-15 =
+* Add support to load images over ssl
+
+= version 1.1.0 =
+* Removed jQuery library to get feeds.
+* Added WordPress cache to better speed.
+* Removed Style file and load it at wp_head before theme style.
+
+= version 1.0.1 =
+* Improved style for better display
+* Added control over number of photo for each row
+
+= version 1.0 =
+Implementation of basic functionality.
+
+== Upgrade Notice ==
+Upgrade the plugin to get latest features.
---
+++ simple-flickr-widget.php
@@ -0,0 +1,195 @@
+<?php
+/**!
+ * Plugin Name: Simple Flickr Widget
+ * Plugin URI: https://wordpress.org/plugins/simple-flickr-widget/
+ * Description: A WordPress widget to display your latest Flickr photos. 
+ * Version: 1.2.0
+ * Author: Sayful Islam
+ * Author URI: https://sayfulislam.com
+ * License: GPL2
+ */
+
+class Simple_Flickr_Widget extends WP_Widget {
+
+	private $widget_id;
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	public function __construct()
+	{
+		$this->widget_id 	= 'flickr_widget';
+		$widget_name 		= __( 'Simple Flickr Widget', 'simple-flickr-widget' );
+		$widget_options 	= array(
+			'classname' 	=> 'simple_flicker_widget',
+			'description' 	=> __( 'Display your latest Flickr photos.', 'simple-flickr-widget' ),
+		);
+
+		parent::__construct( $this->widget_id, $widget_name, $widget_options );
+
+		add_action( 'save_post', array( $this, 'flush_widget_cache' ) );
+		add_action( 'deleted_post', array( $this, 'flush_widget_cache' ) );
+		add_action( 'switch_theme', array( $this, 'flush_widget_cache' ) );
+
+		add_action( 'wp_head', array( $this, 'inline_style' ), 5 );
+	}
+
+	public function flush_widget_cache() {
+		wp_cache_delete( $this->widget_id );
+	}
+
+	public function inline_style()
+	{
+		?>
+		<style type="text/css">
+			.simple_flicker_widget { overflow: hidden; }
+			.simple_flicker_widget-row { margin-left: -5px; margin-right: -5px; }
+			.simple_flicker_widget-row:before, .widget_shapla_flickr .shapla-flickr-row:after { display: table; content: ""; }
+			.simple_flicker_widget-row:after { clear: both; }
+			.simple_flicker_widget-col { padding: 0 5px 10px; float: left; width: 100%; }
+			.simple_flicker_widget-col.col2 { width: 50%; }
+			.simple_flicker_widget-col.col3 { width: 33.333333%; }
+			.simple_flicker_widget-col.col4 { width: 25%; }
+			.simple_flicker_widget-col.col5 { width: 20%; }
+			.simple_flicker_widget-col.col6 { width: 16.666667%; }
+			.simple_flicker_widget-col a { display: block; }
+			.simple_flicker_widget-col img { height: auto; max-width: 100%; width: 100%; }
+		</style>
+		<?php
+	}
+
+	function widget( $args, $instance ) {
+
+		$cache = wp_cache_get( $this->widget_id );
+
+		if ( $cache ){
+			echo $cache;
+			return;
+		}
+
+		ob_start();
+
+		extract( $args );
+		
+		$title 		= isset($instance['title']) ? esc_attr( $instance['title'] ) : null;
+		$flickr_id 	= isset($instance['flickr_id']) ? esc_attr($instance['flickr_id']) : null;
+		$number 	= isset($instance['number']) ? absint($instance['number']) : 9;
+		$row_number = isset($instance['row_number']) ? absint($instance['row_number']) : 3;
+		
+		include_once(ABSPATH . WPINC . '/feed.php');
+
+		$rss = fetch_feed('http://api.flickr.com/services/feeds/photos_public.gne?ids='.$flickr_id.'&lang=en-us&format=rss_200');
+
+		add_filter( 'wp_feed_cache_transient_lifetime', function(){
+			return 1800;
+		});
+
+		if( is_wp_error( $rss ) ){
+			return;
+		}
+
+		// Figure out how many total items there are, but limit it to 5. 
+	    $max_items = $rss->get_item_quantity( $number );
+	    // Build an array of all the items, starting with element 0 (first element).
+		$items = $rss->get_items( 0, $max_items );
+
+		echo $before_widget;
+
+		if ( $title ) echo $before_title . $title . $after_title;
+		?>
+		<div class="simple_flicker_widget-row">
+		<?php
+			if ( isset( $items ) ) {
+				foreach( $items as $item ) {
+					// thumbnail, 
+					$image_group = $item->get_item_tags('http://search.yahoo.com/mrss/', 'thumbnail');
+					$image_attrs = $image_group[0]['attribs'];
+					foreach( $image_attrs as $image ) {
+
+						$_img_src 		= $image['url'];
+						$_img_src 		= str_replace('http://', 'https://', $_img_src );
+						$_img_width 	= intval( $image['width'] );
+						$_img_height 	= intval( $image['height'] );
+
+						echo sprintf('<div class="simple_flicker_widget-col col%1$s"><a target="_blank" href="%2$s"><img src="%3$s" width="%4$s" height="%5$s" alt="%6$s"></a></div>'
+							,$row_number
+							,$item->get_permalink()
+							,$_img_src
+							,$_img_width
+							,$_img_height
+							,$item->get_title()
+						);
+					}
+				}
+			}
+		?>
+		</div>
+		<?php
+		echo $after_widget;
+		$content = ob_get_clean();
+		wp_cache_set( $this->widget_id, $content );
+		echo $content;
+	}
+
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		
+		$instance['title'] 		= sanitize_text_field( $new_instance['title'] );
+		$instance['flickr_id'] 	= sanitize_text_field( $new_instance['flickr_id'] );
+		$instance['number'] 	= absint( $new_instance['number'] );
+		$instance['row_number'] = absint( $new_instance['row_number'] );
+
+		$this->flush_widget_cache();
+
+		return $instance;
+	}
+
+	function form( $instance ){
+		$defaults = array(
+			'title'        	=> __( 'Flickr Photos', 'simple-flickr-widget' ),
+			'flickr_id'    	=> '',
+			'number' 		=> 9,
+			'row_number' 	=> 3,
+		);
+		$instance = wp_parse_args( (array) $instance, $defaults );
+
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>">
+				<?php _e( 'Title:', 'simple-flickr-widget' ); ?>
+			</label>
+			<input type="text" class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo $instance['title']; ?>">
+		</p>
+
+		<p>
+			<label for="<?php echo $this->get_field_id('flickr_id'); ?>">
+				<?php _e( 'Your Flickr User ID:', 'simple-flickr-widget' ); ?>
+			</label>
+			<input type="text" class="widefat" id="<?php echo $this->get_field_id('flickr_id'); ?>" name="<?php echo $this->get_field_name('flickr_id'); ?>" value="<?php echo $instance['flickr_id']; ?>">
+			<span class="description">
+				<?php echo sprintf( __( 'Head over to %s to find your Flickr user ID.', 'simple-flickr-widget' ), '<a href="//idgettr.com" target="_blank" rel="nofollow">idgettr</a>' ); ?>
+			</span>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('number'); ?>">
+				<?php _e( 'Total Number of photos to show:', 'simple-flickr-widget' ); ?>
+			</label>
+			<input type="number" class="widefat" id="<?php echo $this->get_field_id('number'); ?>" name="<?php echo $this->get_field_name('number'); ?>" value="<?php echo $instance['number']; ?>">
+			<span class="description">
+				<?php echo __( 'Set how many photos you want to show. Flickr seems to limit its feeds to 20. So you can use maximum 20 photos.', 'simple-flickr-widget' ); ?>
+			</span>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('row_number'); ?>">
+				<?php _e( 'Number of photos to show per column:', 'simple-flickr-widget' ); ?>
+			</label>
+			<input type="number" class="widefat" id="<?php echo $this->get_field_id('row_number'); ?>" name="<?php echo $this->get_field_name('row_number'); ?>" value="<?php echo $instance['row_number']; ?>">
+			<span class="description">
+				<?php echo __( 'Set how many photos you want to show in a row. You can use minimum 1 photo and maximum 6 photos.', 'simple-flickr-widget' ); ?>
+			</span>
+		</p>
+		<?php
+	}
+}
+
+add_action( 'widgets_init', function(){ register_widget( "Simple_Flickr_Widget" );});
